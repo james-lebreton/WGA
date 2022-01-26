e9563a404c3999d842f01fa9fc1074a7d02bffc9
James M. LeBreton <jlebreto@gmail.com>
2022-01-26 13:56:45 -0500

    tweaking

--- R/2022-01-25 Building & Testing Package.R
+++ R/2022-01-25 Building & Testing Package.R
@@ -1,20 +1,13 @@
 # 2022-01-25
 # James creating package to post to github.
-install.packages("devtools")
-library(devtools)
-install_github("james-lebreton/WGA")
+# 1. Create new repo online at github.com
 
-
-# 1. Open Git Bash
-
-# 2. Change wordking diretory to current project
-### cd D:/Users/James/Dropbox/Rprojects/WGA
-
-# 3. Initialize the local direcdtory as a Git respository
-### git init
-
-# 4. Add the files in the new local repository
-### git add .
-
-# 5. Commit the files I've staged in my local repository
-### git commit -m "First Commit"
+# 2. Follow instructions from github.com
+### basically, just copy & paste the code from
+###   github.com into the shell -- "RStudio --> tools --> shell"
+install.packages("multilevel")
+install.packages("psych")
+library(multilevel)
+data(lq2002, package = "multilevel")
+WGA(x = lq2002[,c("LEAD01")], grpid = lq2002$COMPID, scale = c(1,5),
+               model = "consensus", reset = T)
